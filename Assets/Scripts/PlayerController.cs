2c4adcf1cc9f2e0f3d4cba427d22b0115136e424
Lee Hyoseok <lhs19960330@gmail.com>
2024-02-19 15:12:06 +0900

    feat : 에니매이션(1)

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -8,13 +8,16 @@ public class PlayerController : MonoBehaviour
     [Header("Component")]
     // CharacterController는 월드기준으로 움직인다. 유일하게 transform만 자기자신기준의 좌표를 가진다.
     [SerializeField] CharacterController controller;
+    [SerializeField] Animator animator;
 
     [Header("Spec")]
     [SerializeField] float moveSpeed;
+    [SerializeField] float walkSpeed;
     [SerializeField] float jumpSpeed;
 
     private Vector3 moveDir;
     private float ySpeed;
+    private bool isWalk;
 
     private void Update()
     {
@@ -26,8 +29,29 @@ public class PlayerController : MonoBehaviour
     {
         // CharacterController은 Move함수로 움직인다. (월드 기준으로 움직여서 폐기)
         //controller.Move(moveDir * moveSpeed * Time.deltaTime);
-        controller.Move(transform.right * moveDir.x * moveSpeed * Time.deltaTime);
-        controller.Move(transform.forward * moveDir.z * moveSpeed * Time.deltaTime);
+
+        if (isWalk)
+        {
+            controller.Move(transform.right * moveDir.x * walkSpeed * Time.deltaTime);
+            controller.Move(transform.forward * moveDir.z * walkSpeed * Time.deltaTime);
+            animator.SetFloat("XSpeed", moveDir.x * walkSpeed, 0.1f, Time.deltaTime);
+            animator.SetFloat("YSpeed", moveDir.z * walkSpeed, 0.1f, Time.deltaTime);
+            animator.SetFloat("MoveSpeed", moveDir.magnitude * walkSpeed, 0.1f, Time.deltaTime);
+        }
+        else
+        {
+            // 걸을때와 뛸때를 만들어준다.
+            controller.Move(transform.right * moveDir.x * moveSpeed * Time.deltaTime);
+            controller.Move(transform.forward * moveDir.z * moveSpeed * Time.deltaTime);
+            // 움직일때 애니메이션이 바꿔준다.
+            animator.SetFloat("XSpeed", moveDir.x * moveSpeed, 0.1f, Time.deltaTime);
+            animator.SetFloat("YSpeed", moveDir.z * moveSpeed, 0.1f, Time.deltaTime);
+            animator.SetFloat("MoveSpeed", moveDir.magnitude * moveSpeed, 0.1f, Time.deltaTime);
+        }
+
+
+        //animator.SetFloat("MoveSpeed", moveDir.magnitude * moveSpeed);
+
     }
     private void JumpMove()
     {
@@ -43,8 +67,18 @@ public class PlayerController : MonoBehaviour
         }
 
         controller.Move(Vector3.up * ySpeed * Time.deltaTime);
-    }
 
+    }
+    public void Fire()
+    {
+        // 총쏘는 로직 구현
+        animator.SetTrigger("Fire");
+    }
+    public void Reload()
+    {
+        // 재장전 구현
+        animator.SetTrigger("Reload");
+    }
     private void OnMove(InputValue value)
     {
         Vector2 inputDir = value.Get<Vector2>();
@@ -56,5 +90,24 @@ public class PlayerController : MonoBehaviour
     {
         ySpeed = jumpSpeed;
     }
+    private void OnWalks(InputValue value)
+    {
+        if (value.isPressed)
+        {
+            isWalk = true;
 
+        }
+        else
+        {
+            isWalk = false;
+        }
+    }
+    private void OnFire(InputValue value)
+    {
+        Fire();
+    }
+    private void OnReload(InputValue value)
+    {
+        Reload();   
+    }
 }
